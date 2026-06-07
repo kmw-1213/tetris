309be686debdd69a660de26c95572c835795323a
unknown <k1213mw@gmail.com>
2026-06-07 15:54:45 +0900

    내용 수정

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """main.py: 실행 진입점 (앱 팩토리 호출 + SocketIO 서버 구동)"""
+import os
 from server import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    # SocketIO(멀티플레이) 를 사용하므로 socketio.run 으로 구동
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    debug = os.environ.get('FLASK_ENV') == 'development'
+    socketio.run(app, host='0.0.0.0', port=5000, debug=debug, allow_unsafe_werkzeug=True)
